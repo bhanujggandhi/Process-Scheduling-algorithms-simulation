3e67b9d0b497a0953dc5a4a47ea19db679df6782
bhanujggandhi <gandhibhanuj@gmail.com>
2022-11-27 01:59:28 +0530

    mlfq

--- multi_level_feedback_queue.cpp
+++ multi_level_feedback_queue.cpp
@@ -1,158 +1,101 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <unordered_map>
-#include <vector>
+#include <stdio.h>
 
-using namespace std;
+struct process {
+    char name;
+    int AT, BT, WT, TAT, RT, CT;
+} Q1[10], Q2[10], Q3[10]; /*Three queues*/
 
-/* Type Definition */
-typedef unsigned long long ull;
-typedef long long ll;
-typedef long double lld;
-
-/* UUID Queue */
-
-queue<unsigned int> UUID;
-
-unsigned int getUUID() {
-    if (!UUID.empty()) {
-        unsigned int uid = UUID.front();
-        UUID.pop();
-        return uid;
-    }
-
-    return rand();
-}
-
-/* DataTypes */
-
-enum States {
-    NEW,
-    READY,
-    RUNNING,
-    WAITING,
-    TERMINATED
-};
-
-struct PCB {
-    unsigned int pid;
-    string pname;
-    bool ptype; /* 0->IO, 1->CPU */
-    int priority;
-    lld arrival_time;
-    lld burst_time1;
-    lld io_time;
-    lld burst_time2;
-    lld completion_time;
-    lld turnaround_time;
-    lld waiting_time;
-    lld response_time;
-    States state;
-
-    PCB(string _pname, lld _arrival_time, lld _burst_time1, lld _io_time, lld _burst_time2, lld _priority) {
-        pid = getUUID();
-        pname = _pname;
-        ptype = _pname[0] == 'I' ? 0 : 1;
-        priority = _priority;
-        arrival_time = _arrival_time;
-        burst_time1 = _burst_time1;
-        io_time = _io_time;
-        burst_time2 = _burst_time2;
-        completion_time = INT64_MIN;
-        turnaround_time = INT64_MIN;
-        waiting_time = INT64_MIN;
-        response_time = INT64_MIN;
-        state = NEW;
-    }
-
-    ~PCB() {
-        UUID.push(pid);
-    }
-};
-
-struct CompareProcessMLFQ {
-    bool operator()(PCB *const &p1, PCB *const &p2) {
-        if (p1->priority == p2->priority) {
-            if (p1->arrival_time == p2->arrival_time) {
-                if (p1->ptype == 0 and p2->ptype == 0) {
-                    return p1->pid > p2->pid;
-                } else if (p1->ptype == 0) {
-                    return false;
-                } else {
-                    return true;
-                }
+int n;
+void sortByArrival() {
+    struct process temp;
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (Q1[i].AT > Q1[j].AT) {
+                temp = Q1[i];
+                Q1[i] = Q1[j];
+                Q1[j] = temp;
             }
-            return p1->arrival_time > p2->arrival_time;
         }
-        return p1->priority < p2->priority;
     }
-};
-
-/* Global Queues */
-vector<PCB *> processes;
-unordered_map<int, PCB *> process_map;
-unordered_map<unsigned int, PCB *> MLFQRunning_map;
-priority_queue<PCB *, vector<PCB *>, CompareProcessMLFQ> MLFQ_Ready_queue;
-priority_queue<PCB *, vector<PCB *>, CompareProcessMLFQ> MLFQ_Device_queue;
-
-/* Utility Functions */
-vector<PCB *> MLFQ() {
-    vector<PCB *> MLFQVect;
-
-    return MLFQVect;
 }
-void context_switch();
-void scheduleMLFQ();
+
 int main() {
-    for (int i = 0; i < 500; i++) {
-        UUID.push(i);
+    int i, j, k = 0, r = 0, time = 0, tq1 = 5, tq2 = 8, flag = 0;
+    char c;
+    printf("Enter no of processes:");
+    scanf("%d", &n);
+    for (i = 0, c = 'A'; i < n; i++, c++) {
+        Q1[i].name = c;
+        printf("\nEnter the arrival time and burst time of process %c: ", Q1[i].name);
+        scanf("%d%d", &Q1[i].AT, &Q1[i].BT);
+        Q1[i].RT = Q1[i].BT; /*save burst time in remaining time for each process*/
     }
-    PCB *IdleProcess1 = new PCB("IIdle1", 0, 0, 0, INT64_MAX, -1);
+    sortByArrival();
+    time = Q1[0].AT;
+    printf("Process in first queue following RR with qt=5");
+    printf("\nProcess\t\tRT\t\tWT\t\tTAT\t\t");
+    for (i = 0; i < n; i++) {
 
-    int n;
-    cout << "Enter the Number of process: ";
-    cin >> n;
-    string ProcessName;
-    lld ArrivalTime, BurstTime1, IOTime, BurstTime2, Priority;
-    cout << "\nInput format should be :  ProcessName  ArrivalTime  BurstTime1  IOTime  BurstTime2  QueueNumber(1 or 2)\n  ";
+        if (Q1[i].RT <= tq1) {
 
-    for (int i = 0; i < n; i++) {
-        cin >> ProcessName;
-        cin >> ArrivalTime >> BurstTime1 >> IOTime >> BurstTime2 >> Priority;
-        PCB *temp = new PCB(ProcessName, ArrivalTime, BurstTime1, IOTime, BurstTime2, Priority);
-        processes.push_back(temp);
+            time += Q1[i].RT; /*from arrival time of first process to completion of this process*/
+            Q1[i].RT = 0;
+            Q1[i].WT = time - Q1[i].AT - Q1[i].BT; /*amount of time process has been waiting in the first queue*/
+            Q1[i].TAT = time - Q1[i].AT;           /*amount of time to execute the process*/
+            printf("\n%c\t\t%d\t\t%d\t\t%d", Q1[i].name, Q1[i].BT, Q1[i].WT, Q1[i].TAT);
+
+        } else /*process moves to queue 2 with qt=8*/
+        {
+            Q2[k].WT = time;
+            time += tq1;
+            Q1[i].RT -= tq1;
+            Q2[k].BT = Q1[i].RT;
+            Q2[k].RT = Q2[k].BT;
+            Q2[k].name = Q1[i].name;
+            k = k + 1;
+            flag = 1;
+        }
+    }
+    if (flag == 1) {
+        printf("\nProcess in second queue following RR with qt=8");
+        printf("\nProcess\t\tRT\t\tWT\t\tTAT\t\t");
+    }
+    for (i = 0; i < k; i++) {
+        if (Q2[i].RT <= tq2) {
+            time += Q2[i].RT; /*from arrival time of first process +BT of this process*/
+            Q2[i].RT = 0;
+            Q2[i].WT = time - tq1 - Q2[i].BT; /*amount of time process has been waiting in the ready queue*/
+            Q2[i].TAT = time - Q2[i].AT;      /*amount of time to execute the process*/
+            printf("\n%c\t\t%d\t\t%d\t\t%d", Q2[i].name, Q2[i].BT, Q2[i].WT, Q2[i].TAT);
+
+        } else /*process moves to queue 3 with FCFS*/
+        {
+            Q3[r].AT = time;
+            time += tq2;
+            Q2[i].RT -= tq2;
+            Q3[r].BT = Q2[i].RT;
+            Q3[r].RT = Q3[r].BT;
+            Q3[r].name = Q2[i].name;
+            r = r + 1;
+            flag = 2;
+        }
     }
 
-    return 0;
-}
-
-void context_switch(PCB *currp, States st) {
-    currp->state = st;
-}
-
-void scheduleMLFQ() {
-    // 0 pid is for idle process
-    if (MLFQRunning_map.size() == 1 and MLFQRunning_map.find(0) == MLFQRunning_map.end()) {
-        return;
+    {
+        if (flag == 2)
+            printf("\nProcess in third queue following FCFS ");
     }
-    // If only idle process is present and some other process comes
-    if (MLFQ_Ready_queue.size() >= 1 and MLFQRunning_map.size() == 1 and MLFQRunning_map.find(0) != MLFQRunning_map.end()) {
-        auto idlep = MLFQRunning_map[0];
-        MLFQRunning_map.erase(0);
-        MLFQ_Ready_queue.push(idlep);
-        context_switch(idlep, READY);
-
-        auto currprocess = MLFQ_Ready_queue.top();
-        MLFQ_Ready_queue.pop();
-        MLFQRunning_map.insert(make_pair(currprocess->pid, currprocess));
-        context_switch(currprocess, RUNNING);
+    for (i = 0; i < r; i++) {
+        if (i == 0)
+            Q3[i].CT = Q3[i].BT + time - tq1 - tq2;
+        else
+            Q3[i].CT = Q3[i - 1].CT + Q3[i].BT;
     }
-    // If running queue is empty, schedule any process in pq
-    else if (MLFQRunning_map.size() == 0) {
-        auto currprocess = MLFQ_Ready_queue.top();
-        MLFQ_Ready_queue.pop();
-        MLFQRunning_map.insert(make_pair(currprocess->pid, currprocess));
-        context_switch(currprocess, RUNNING);
+
+    for (i = 0; i < r; i++) {
+        Q3[i].TAT = Q3[i].CT;
+        Q3[i].WT = Q3[i].TAT - Q3[i].BT;
+        printf("\n%c\t\t%d\t\t%d\t\t%d\t\t", Q3[i].name, Q3[i].BT, Q3[i].WT, Q3[i].TAT);
     }
 }
